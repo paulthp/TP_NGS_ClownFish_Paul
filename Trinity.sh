e7a386cc797b9993ac6191ab715f4b19c833a722
paul thienpont <paul@thienpont.fr>
2020-11-17 15:39:03 +0000

    little modifications

--- Trinity.sh
+++ Trinity.sh
@@ -12,9 +12,9 @@ cd $data
 #| = on prend le resultat et on l'envoie dans la suite
 #avec paste on colle les noms en les séparant avec une virgule (besoin pour trinity)
 FASTQ=$(ls $data/sar_data/*.fastq |paste -s -d, -)
-echo $FASTQ
 
-#Trinity --seqType fq --max_memory 50G --single FASTQ --SS_lib_type R --output sra_data_Trinity --CPU 14 --normalize_by_read_set
+
+Trinity --seqType fq --max_memory 50G --single FASTQ --SS_lib_type R --output sra_data_Trinity --CPU 14 --normalize_by_read_set
 
 
 # Remove length and path in sequence names to avoid bug with blast ( sequence name length > 1000)
