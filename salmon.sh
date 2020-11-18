8c19f82b15b788e9cb08ce6bfb56e1f2aadd840d
paul thienpont <paul@thienpont.fr>
2020-11-18 14:49:42 +0000

    add a few commentaries

--- salmon.sh
+++ salmon.sh
@@ -13,9 +13,9 @@ mkdir -p data_salmon
 salmon index -k 25 -t $data/sra_data_Trinity/Trinity.fasta -i $data/data_salmon/trinity_index
 
 # Quantification
-# Dans Name on met le nom des variables sans .fastq pour pouvoir créer un dossier à chaque boucle
+# Dans Name on met le nom des variables sans .fastq pour pouvoir créer un dossier à chaque boucle du nom du fichier sans fastq (pas beau)
 # Pour éviter que a chaque boucle on remplace les données d'avant
-#-p numbre of threads = nombre de coeurs
+#-p numbre of threads = nombre de coeurs (pour l'ordinateur)
 
 fastq=$(ls $data/sra_data/*.fastq)
 echo $fastq
